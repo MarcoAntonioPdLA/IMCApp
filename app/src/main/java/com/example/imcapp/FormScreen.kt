2d9a2ccde3e057e014db0e586523e97393311246
MarcoAntonioPdLA <mponcedeleona@unsa.edu.pe>
2026-04-28 13:57:54 -0500

    Creando primera parte del formulario.

--- app/src/main/java/com/example/imcapp/FormScreen.kt
+++ app/src/main/java/com/example/imcapp/FormScreen.kt
@@ -1,21 +1,88 @@
 package com.example.imcapp
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.imcapp.ui.theme.Typography
 
 @Composable
 fun FormScreen(navController: NavController) {
-    Column {
-        Text("Primera pantalla")
-        Button(
-            onClick = {
-                navController.navigate(Routes.resultScreen + "/Marco")
-            }
-        ) {
-            Text("Ir a la segunda pantalla")
+    var genderSelectedIndex: Int by remember { mutableIntStateOf(0) }
+    val genderOptions = listOf("Hombre", "Mujer")
+    var height: Int by remember { mutableIntStateOf(150) }
+    var weight: Int by remember { mutableIntStateOf(50) }
+    var age: Int by remember { mutableIntStateOf(15) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 16.dp, end = 16.dp, top = 24.dp, bottom = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Column {
+            Text(
+                text = "Índice de Masa Corporal",
+                style = Typography.headlineLarge
+            )
+            Text(
+                text = "Ingrese y seleccione la siguiente información:",
+                style = Typography.bodyLarge
+            )
         }
+
+        // Elección de género
+        SingleChoiceSegmentedButtonRow(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            genderOptions.forEachIndexed { index, label ->
+                SegmentedButton(
+                    selected = index == genderSelectedIndex,
+                    onClick = {
+                        genderSelectedIndex = index
+                    },
+                    shape = SegmentedButtonDefaults.itemShape(
+                        index = index,
+                        count = genderOptions.size
+                    )
+                ) {
+                    Text(label)
+                }
+            }
+        }
+
+        //Elección de altura
+        Column {
+            Text(
+                text = "Altura: $height cm",
+                style = Typography.bodyLarge
+            )
+            Slider(
+                value = height.toFloat(),
+                onValueChange = { height = it.toInt() },
+                valueRange = 30f..250f,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+
+        //Elección de peso
+
+
+        //Elección de edad
+
     }
 }
--- app/src/main/java/com/example/imcapp/ResultScreen.kt
+++ app/src/main/java/com/example/imcapp/ResultScreen.kt
@@ -2,7 +2,6 @@ package com.example.imcapp
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
 
 @Composable
 fun ResultScreen(nombre: String) {
